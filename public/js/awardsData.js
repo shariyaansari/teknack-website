6bff3e4216144fc7d8d17129e638fbbd31713f76
grejo.j <grejo.j@media.net>
2024-02-24 18:23:55 +0530

    fix: Migrate Hall of Fame

---
+++ public/js/awardsData.js
@@ -0,0 +1,35 @@
+export default awardsData = {
+    "2024": [
+        {
+            awardTitle: "Best Game UI",
+            gameName: "Rina : The Lost Soul",
+            devNames: "Arkaprabha Ghosh, Nathan Pimenta, Shaun Rodrigues",
+            gameLink: "https://play.google.com/store/apps/details?id=com.teknack.rina",
+            gameIcon: "img/logos-24/rina.webp",
+        },
+        {
+            awardTitle: "Best GamePlay",
+            gameName: "Starfall : Odyssey",
+            devNames: "Nishith Lopes, Raghav Ghoil, Sahil Khude",
+            gameLink:
+                "https://play.google.com/store/apps/details?id=com.teknack.starfall",
+            gameIcon: "img/logos-24/StarfallOdyssey.webp",
+        },
+        {
+            awardTitle: "Best Game Concept",
+            gameName: "Circle Smash",
+            devNames: "Jess John",
+            gameLink:
+                "https://play.google.com/store/apps/details?id=com.com.Teknack.CircleSmash.CircleSmash",
+            gameIcon: "img/logos-24/CircleSmash.webp",
+        },
+        {
+            awardTitle: "People's Choice",
+            gameName: "Blockade Brio",
+            devNames: "Shweta Nadar",
+            gameLink:
+                "https://play.google.com/store/apps/details?id=com.SoloGameDev.teknack",
+            gameIcon: "img/logos-24/BlockadeBrio.webp",
+        },
+    ],
+};
