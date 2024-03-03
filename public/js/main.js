7bffe2c980c41a3dbb4b452bc7899455de937445
Grejo Joby <51746129+grejojoby@users.noreply.github.com>
2024-03-04 00:26:40 +0530

    feat: Gallery from graphcms

--- public/js/main.js
+++ public/js/main.js
@@ -39,13 +39,6 @@
         $(this).css('background-image', 'url(' + bg + ')');
     });
 
-    //Masonary
-    $('.work__gallery').masonry({
-        itemSelector: '.work__item',
-        columnWidth: '.grid-sizer',
-        gutter: 10
-    });
-
     /*------------------
         Navigation
     --------------------*/
