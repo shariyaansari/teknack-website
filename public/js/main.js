e75bd526a0d43a3b04c1575c75303c2ab786685a
grejo.j <grejo.j@media.net>
2024-03-20 17:23:26 +0530

    fix: faster background image loading

--- public/js/main.js
+++ public/js/main.js
@@ -24,14 +24,6 @@
     });
 
     /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    /*------------------
         Navigation
     --------------------*/
     $(".mobile-menu").slicknav({
